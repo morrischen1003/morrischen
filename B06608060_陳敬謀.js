fba84622c1fae82dd71d2edc0958b145853e5c6c
morrischen1003 <66121699+morrischen1003@users.noreply.github.com>
2020-06-21 13:16:37 +0800

    Add files via upload

--- B06608060_陳敬謀.js
+++ B06608060_陳敬謀.js
@@ -1,26 +1,28 @@
-function getinput()
-{
+$(()=>{  //'當文件準備好時執行{}\
+alert('準備好了嗎?');
+function getinput(){
   var res;
   var conf;
-  var res = prompt("Please enter your name.", "");
+  var res = prompt("先填上您的大名", "");
 
   if (res != null)
   {
     if (res != "")
     {
-      conf = confirm("Your name is " + res);
+      conf = confirm("歡迎蒞臨! " + res);
       if (conf == true) 
-        document.write("Welcome, " + res);
+        alert("感謝您的蒞臨!  " + res);
       else
-        document.write("See you next time.");
+        alert("Hello whoever you are.");
     }
     else
-      document.write("Hello whoever you are.");
+      alert("Hello whoever you are.");
   }
   else // if Cancel was clicked
   {
-    document.write("See you next time.");
+    alert("See you next time.");
   }
 }
 
 getinput();
+})
